a54b6c4e2dffb0354ab048a565fffa27595dab8f
Minyong Li <minyong.li@iota.org>
2021-12-31 15:15:39 +0000

    fix seed generation

--- src/account.rs
+++ src/account.rs
@@ -6,25 +6,30 @@ pub struct ChrysalisAccount {
 
 impl ChrysalisAccount {
     pub fn new() -> Self {
-        let mut seed = Vec::new();
-        seed.resize(32, 0);
-        crypto::utils::rand::fill(&mut seed).unwrap();
+        let mut entropy = [0u8; 32];
+        crypto::utils::rand::fill(&mut entropy).unwrap();
 
-        let mnemonics =
-            crypto::keys::bip39::wordlist::encode(&seed, &crypto::keys::bip39::wordlist::ENGLISH)
-                .unwrap();
+        let mnemonics = crypto::keys::bip39::wordlist::encode(
+            &entropy,
+            &crypto::keys::bip39::wordlist::ENGLISH,
+        )
+        .unwrap();
 
-        Self { seed, mnemonics }
+        let mut seed = [0u8; 64];
+        crypto::keys::bip39::mnemonic_to_seed(&mnemonics, "", &mut seed);
+
+        Self {
+            seed: seed.to_vec(),
+            mnemonics,
+        }
     }
 
     pub fn from_mnemonics(mnemonics: &str) -> Result<Self, crypto::keys::bip39::wordlist::Error> {
-        let seed = crypto::keys::bip39::wordlist::decode(
-            mnemonics,
-            &crypto::keys::bip39::wordlist::ENGLISH,
-        )?;
+        let mut seed = [0u8; 64];
+        crypto::keys::bip39::mnemonic_to_seed(mnemonics, "", &mut seed);
 
         Ok(Self {
-            seed,
+            seed: seed.to_vec(),
             mnemonics: mnemonics.to_string(),
         })
     }
