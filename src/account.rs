0cbef4e36ba8970af9a5ea3449c1fd54ef5895f1
Thoralf-M <thoralf.mue@gmail.com>
2022-01-01 11:42:45 +0100

    add mnemonic validation and trim input

--- src/account.rs
+++ src/account.rs
@@ -25,8 +25,13 @@ impl ChrysalisAccount {
     }
 
     pub fn from_mnemonic(mnemonic: &str) -> Result<Self, crypto::keys::bip39::wordlist::Error> {
+        crypto::keys::bip39::wordlist::verify(
+            mnemonic.trim(),
+            &crypto::keys::bip39::wordlist::ENGLISH,
+        )
+        .unwrap();
         let mut seed = [0u8; 64];
-        crypto::keys::bip39::mnemonic_to_seed(mnemonic, "", &mut seed);
+        crypto::keys::bip39::mnemonic_to_seed(mnemonic.trim(), "", &mut seed);
 
         Ok(Self {
             seed: seed.to_vec(),
